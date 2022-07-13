3e9925340e1a7eb4787dd0b4c238ad1b0405f78b
)- <a-@-s-MacBook-Pro.local>
2022-07-13 11:54:44 -0400

    models connected to database, and models connected with views, which now lets us display data from our database on our UI

--- main_app/views.py
+++ main_app/views.py
@@ -1,7 +1,7 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 #from django.views.generic.detail import ListView
-#from .models import Crypto
+from .models import Crypto
 
 # Create your views here.
 def home(request):
@@ -10,7 +10,7 @@ def home(request):
 
 def about(request):
     return render(request, 'about.html')
-
+'''
 #Seed Data
 class Crypto:
     def __init__(self, name, price, rank):
@@ -30,6 +30,7 @@ cryptos =[
     Crypto('Polkadot', '$7.00', 9),
     Crypto('Shiba Inu', '$0.000010', 10),
     ]
-
+'''
 def crypto_index(request):
+    cryptos = Crypto.objects.all()
     return render(request, 'cryptos/index.html', {'cryptos' : cryptos})
