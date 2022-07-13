7cc7a4bd0c09929032a29bd585e8754f32b42edf
)- <a-@-s-MacBook-Pro.local>
2022-07-13 10:40:46 -0400

    seed data added and basic template, basic data can now be rendered in the UI

--- main_app/urls.py
+++ main_app/urls.py
@@ -4,4 +4,5 @@ from .import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('about/', views.about, name='about'),
+    path('crypto/', views.crypto_index, name='index'),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,6 +1,7 @@
 from django.http import HttpResponse
 from django.shortcuts import render
-
+#from django.views.generic.detail import ListView
+#from .models import Crypto
 
 # Create your views here.
 def home(request):
@@ -9,3 +10,26 @@ def home(request):
 
 def about(request):
     return render(request, 'about.html')
+
+#Seed Data
+class Crypto:
+    def __init__(self, name, price, rank):
+        self.name = name
+        self.price = price
+        self.rank = rank
+
+cryptos =[
+    Crypto('Bitcoin', '$20,000', 1),
+    Crypto('Ethereum', '$1,000', 2),
+    Crypto('Tether', '$1.00', 3),
+    Crypto('USD', '$1.00', 4),
+    Crypto('BNB', '$200.00', 5),
+    Crypto('Xrp', '$0.30', 6),
+    Crypto('Cardano', '$0.40', 7),
+    Crypto('Dogecoin', '$0.05', 8),
+    Crypto('Polkadot', '$7.00', 9),
+    Crypto('Shiba Inu', '$0.000010', 10),
+    ]
+
+def crypto_index(request):
+    return render(request, 'cryptos/index.html', {'cryptos' : cryptos})
