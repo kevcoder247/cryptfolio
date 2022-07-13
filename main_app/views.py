e352356ba2835ecfab1b46985e1e74c78b2138c5
)- <a-@-s-MacBook-Pro.local>
2022-07-13 07:21:27 -0400

    about template added and can now be rendered

--- main_app/views.py
+++ main_app/views.py
@@ -1,5 +1,11 @@
 from django.http import HttpResponse
+from django.shortcuts import render
+
 
 # Create your views here.
 def home(request):
-    return HttpResponse("<h1>Hello World</h1>")
+    return HttpResponse("<h1>Home Page</h1>")
+
+
+def about(request):
+    return render(request, 'about.html')
